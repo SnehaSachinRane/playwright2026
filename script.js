063d6c2bb82196a4c207457981be898d2970f601
SnehaR <snehapatil2502@gmail.com>
2026-04-20 21:20:29 +0530

    First JS code

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("This is my first Javascript program!!")
+console.log("This is my second Javascript program!!")
+var value=4;
+const s="Rane";
+let Fname="SnehaR"
+console.log(value)
+console.log(s)
+console.log(Fname)
+s="Patil";
